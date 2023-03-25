a6e515b21b7205550ad9d608c64d8f0cabc6ef1b
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-25 21:41:34 +0200

    test1.4

--- src/js/fetchCounters.js
+++ src/js/fetchCounters.js
@@ -1,3 +1,3 @@
 export default function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,flag`);
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,flag,population,languages`);
 }
