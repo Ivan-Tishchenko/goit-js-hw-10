d85190ecd33f1a8ae7a9fc78cf0e2f9ce11ce80a
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-25 20:33:29 +0200

    test 1.1

--- src/js/build.js
+++ src/js/build.js
@@ -1,3 +1,7 @@
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+
 export default function build(obj) {
     if (obj.length === 1) {
        countryList.insertAdjacentHTML("afterbegin", `<svg class="flag" width="100" height="100">
